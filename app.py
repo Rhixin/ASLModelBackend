b331a884e65e2fc22c996743c2dd80f0d327827c
Rhixin <vallestedted@gmail.com>
2025-04-20 00:22:26 +0800

    Removed gevent

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    socketio.run(app, debug=False, host='0.0.0.0', port=port)
+    # Use socketio directly without gevent/eventlet
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
